d3eceda1f710e67906e9c9b886fa778751301df1
sheroux1 <sheroux@gmail.com>
2022-08-27 16:29:11 -0500

    Supper Time backend

---
+++ app/api/routes.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Recipe, recipe_schema, recipes_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/recipes', methods= ['POST'])
+@token_required
+def store_recipe(current_user_token):
+    recipe_name = request.json['recipe_name']
+    api_id = request.json['api_id']
+    user_token = current_user_token.token
+
+    print(f'Current token: {current_user_token.token}')
+
+    recipe = Recipe(recipe_name, api_id, user_token=user_token)
+
+    db.session.add(recipe)
+    db.session.commit()
+
+    response = recipe_schema.dump(recipe)
+    return jsonify(response)
+
+@api.route('/recipes', methods= ['GET'])
+@token_required
+def get_recipes(current_user_token):
+    a_user = current_user_token.token
+    recipes = Recipe.query.filter_by(user_token = a_user).all()
+    response = recipes_schema.dump(recipes)
+    return jsonify(response)
+
+@api.route('/recipes/<id>', methods= ['GET'])
+@token_required
+def get_single_recipe(current_user_token, id):
+    recipe = Recipe.query.get(id)
+    response = recipe_schema.dump(recipe)
+    return jsonify(response)
+
+@api.route('/recipes/<id>', methods= ['POST', 'PUT'])
+@token_required
+def update_recipe(current_user_token, id):
+    recipe = Recipe.query.get(id)
+    recipe.recipe_name = request.json['recipe_name']
+    recipe.api_id = request.json['api_id']
+    recipe.user_token = current_user_token.token
+
+    db.session.commit()
+    response = recipe_schema.dump(recipe)
+    return jsonify(response)
+
+@api.route('/recipes/<id>', methods= ['DELETE'])
+@token_required
+def delete_recipe(current_user_token, id):
+    recipe = Recipe.query.get(id)
+    db.session.delete(recipe)
+    db.session.commit()
+    response = recipe_schema.dump(recipe)
+    return jsonify(response)
